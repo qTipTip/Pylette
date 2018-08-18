5220d81456120f3ae295e3c7033ea66f4d7f4c64
Ivar Stangeby <istangeby@gmail.com>
2018-08-18 16:46:04 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Pylette',
-    version='0.2',
+    version='0.3',
     packages=['Pylette'],
     url='https://github.com/qTipTip/Pylette',
     license='MIT',
