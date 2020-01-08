7a34e6e3ef2050fa32186ff7f31ec5f6409268bb
ivar <istangeby@gmail.com>
2020-01-08 17:13:41 +0100

    Added display image argument.

--- Pylette/cmd.py
+++ Pylette/cmd.py
@@ -4,19 +4,27 @@ from Pylette import extract_colors
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('filename', help='path to image file', type=str)
+    parser.add_argument('--mode', help='extraction_mode (KMeans/MedianCut', type=str,
+                        default='KM', choices=['KM', 'MC'])
     parser.add_argument('--n', help='the number of colors to extract',
                         type=int, default=5)
-    parser.add_argument('--sort_by', help='sort by luminance or frequency', default='luminance', type=str)
+    parser.add_argument('--sort_by', help='sort by luminance or frequency', default='luminance', type=str,
+                        choices=['luminance', 'frequency'])
     parser.add_argument('--stdout', help='whether to display the extracted color values in the stdout', type=bool,
                         default=True)
-    parser.add_argument('--colorspace', help='which color-space to represent the colors in', default='RGB', type=str)
+    parser.add_argument('--colorspace', help='color space to represent colors in', default='RGB', type=str,
+                        choices=['rgb', 'hsv', 'hls'])
     parser.add_argument('--out_filename', help='where to save the csv file', default=None, type=str)
+    parser.add_argument('--display-colors', help='Open a window displaying the extracted palette', default=False,
+                        type=bool)
     args = parser.parse_args()
     palette = extract_colors(args.filename, palette_size=args.n, sort_mode=args.sort_by)
 
-    palette.to_csv(filename=args.out_filename, frequency='True')
+    palette.to_csv(filename=args.out_filename, frequency='True', stdout=args.stdout)
+    if args.display_colors:
+        palette.display()
 
 
 if __name__ == '__main__':
