1d72cbdd48bfc0a492aeeee7f970dcce5e8b7ba2
Ivar Stangeby <istangeby@gmail.com>
2020-04-02 07:32:37 -0700

    Image resizes to fill the screen.

--- Pylette/gui.py
+++ Pylette/gui.py
@@ -1,6 +1,26 @@
 import sys
 
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QFileDialog, QAction
+import PyQt5
+from PyQt5.QtGui import QPixmap, QPainter
+
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QFileDialog, QAction, QSlider, QVBoxLayout, QHBoxLayout, \
+    QWidget
+
+
+class ResizingLabel(QWidget):
+    def __init__(self, parent=None, *args, **kwargs):
+        QWidget.__init__(self, parent=parent, *args, **kwargs)
+        self.p = QPixmap()
+
+    def setPixmap(self, p):
+        self.p = p
+        self.update()
+
+    def paintEvent(self, event):
+        if not self.p.isNull():
+            painter = QPainter(self)
+            painter.setRenderHint(QPainter.SmoothPixmapTransform)
+            painter.drawPixmap(self.rect(), self.p)
 
 
 class MainWindow(QMainWindow):
@@ -17,6 +37,17 @@ class MainWindow(QMainWindow):
         openAction.triggered.connect(self.open_image)
         filemenu.addAction(openAction)
 
+        self.image_label = ResizingLabel()
+        self.palette_size = QSlider(PyQt5.QtCore.Qt.Horizontal)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.palette_size)
+        layout.addWidget(self.image_label)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+
+        self.setCentralWidget(widget)
         self.resize(500, 500)
 
     def open_image(self):
@@ -24,7 +55,7 @@ class MainWindow(QMainWindow):
         filename = QFileDialog.getOpenFileName()
         imagePath = filename[0]
 
-        print(imagePath)
+        self.image_label.setPixmap(QPixmap(imagePath))
 
 
 def main():
