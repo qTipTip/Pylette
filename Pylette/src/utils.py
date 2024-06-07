bfbbfe4d896dd0de3555ad52f742b9fac9e0a6cd
Ivar Stangeby <ivar.stangeby@cognite.com>
2024-06-07 19:52:05 +0200

    add type hints

--- Pylette/src/utils.py
+++ Pylette/src/utils.py
@@ -1,28 +1,25 @@
 import numpy as np
+from numpy.typing import NDArray
 
 
-class ColorBox(object):
+class ColorBox:
     """
     Represents a box in the RGB color space, with associated attributes, used in the Median Cut algorithm.
     """
 
-    def __init__(self, colors):
+    def __init__(self, colors: NDArray[np.uint8, ..., 3]):
         """
         Initialize with a numpy array of RGB colors.
         :param colors: np.ndarray (width * height, 3)
         """
-
         self.colors = colors
         self._get_min_max()
 
-    def _get_min_max(self):
-        min_channel = np.min(self.colors, axis=0)
-        max_channel = np.max(self.colors, axis=0)
+    def _get_min_max(self) -> None:
+        self.min_channel: NDArray[np.uint8, (3,)] = np.min(self.colors, axis=0)
+        self.max_channel: NDArray[np.uint8, (3,)] = np.max(self.colors, axis=0)
 
-        self.min_channel = min_channel
-        self.max_channel = max_channel
-
-    def __lt__(self, other):
+    def __lt__(self, other: "ColorBox") -> bool:
         """
         Compare cubes by volume
         :param other:
@@ -30,40 +27,34 @@ class ColorBox(object):
         return self.size < other.size
 
     @property
-    def size(self):
+    def size(self) -> np.uint64:
         return self.volume
 
-    def _get_dominant_channel(self):
-        dominant_channel = np.argmax(self.max_channel - self.min_channel)
+    def _get_dominant_channel(self) -> int:
+        diff: NDArray[np.uint8, (3,)] = self.max_channel - self.min_channel
+        dominant_channel = np.argmax(diff)
         return dominant_channel
 
     @property
-    def average(self):
+    def average(self) -> NDArray[np.uint8, (3,)]:
         """
         Returns the average color contained in ColorBox
-        :return: [R, G, B]
+        :return: np.array([R, G, B])
         """
-
         return np.mean(self.colors, axis=0)
 
     @property
-    def volume(self):
-        return np.prod(
-            self.max_channel - self.min_channel,
-        )
+    def volume(self) -> np.uint64:
+        diff: NDArray[np.uint8, (3,)] = self.max_channel - self.min_channel
+        return np.prod(diff).item()
 
-    def split(self):
+    def split(self) -> list["ColorBox"]:
         """
         Splits the ColorBox into two ColorBoxes at the median of the dominant color channel.
         :return: [ColorBox1, ColorBox2]
         """
-
-        # get the color channel with highest range
         dominant_channel = self._get_dominant_channel()
-
-        # sorting colors by the dominant channel
         self.colors = self.colors[self.colors[:, dominant_channel].argsort()]
-
         median_index = len(self.colors) // 2
 
         return [
