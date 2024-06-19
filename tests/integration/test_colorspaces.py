b7d537c17a6c471ffc5e7ad39aa73079eef4df7e
Ivar Stangeby <istangeby@gmail.com>
2024-06-19 09:31:16 +0200

    [feat] Add support for image as numpy array (#31)
    * Add image as array option
    
    * Change version, and update changelog

--- tests/integration/test_colorspaces.py
+++ tests/integration/test_colorspaces.py
@@ -1,5 +1,6 @@
 import pathlib
 
+import cv2
 import pytest
 from numpy.testing import assert_approx_equal
 
@@ -21,10 +22,16 @@ def test_image_as_bytes():
         yield f.read()
 
 
+@pytest.fixture
+def test_image_from_opencv():
+    test_image = pathlib.Path(__file__).parent.parent / "data/test_image.png"
+    return cv2.imread(str(test_image.absolute().resolve()))
+
+
 @pytest.fixture()
 def test_kmean_extracted_palette(test_image_path_as_str):
     return extract_colors(
-        image=test_image_path_as_str, palette_size=10, mode="KM", resize=True
+        image=test_image_path_as_str, palette_size=10, resize=True, mode="KM"
     )
 
 
@@ -47,8 +54,8 @@ def test_palette_invariants_with_image_path(
     palette = extract_colors(
         image=test_image_path_as_str,
         palette_size=palette_size,
-        mode=extraction_mode,
         resize=True,
+        mode=extraction_mode,
     )
 
     assert (
@@ -96,8 +103,57 @@ def test_palette_invariants_with_image_bytes(
     palette = extract_colors(
         image_bytes=test_image_as_bytes,
         palette_size=palette_size,
-        mode=extraction_mode,
         resize=True,
+        mode=extraction_mode,
+    )
+
+    assert (
+        len(palette) == palette_size
+    ), f"Expected {palette_size} colors in palette, got {len(palette)}"
+    assert (
+        palette.number_of_colors == palette_size
+    ), f"Expected {palette_size} colors in palette, got {palette.number_of_colors}"
+    assert (
+        len(palette.colors) == palette_size
+    ), f"Expected {palette_size} colors in palette, got {len(palette.colors)}"
+    assert (
+        palette.colors[0].freq >= palette.colors[-1].freq
+    ), "Expected colors to be sorted by frequency in descending order"
+    assert (
+        palette.colors[0].freq > 0.0
+    ), "Expected the most frequent color to have a frequency greater than 0.0"
+    assert (
+        palette.colors[0].freq <= 1.0
+    ), "Expected the most frequent color to have a frequency less than or equal to 1.0"
+
+    assert_approx_equal(
+        sum(c.freq for c in palette.colors),
+        1.0,
+        err_msg="Expected the sum of all frequencies to be 1.0",
+    )
+
+
+@pytest.mark.parametrize("palette_size", [1, 5, 10, 100])
+@pytest.mark.parametrize(
+    "extraction_mode",
+    [
+        "KM",
+        pytest.param(
+            "MC",
+            marks=pytest.mark.skip(
+                "Currently a bug in the MC algorithm, causing frequencies not summing to one"
+            ),
+        ),
+    ],
+)
+def test_palette_invariants_with_opencv(
+    test_image_from_opencv, palette_size, extraction_mode
+):
+    palette = extract_colors(
+        image_array=test_image_from_opencv,
+        palette_size=palette_size,
+        resize=True,
+        mode=extraction_mode,
     )
 
     assert (
@@ -159,15 +215,15 @@ def test_color_extraction_deterministic_kmeans(
     palette1 = extract_colors(
         image=test_image_path_as_str,
         palette_size=5,
-        mode="KM",
         resize=resize,
+        mode="KM",
         sort_mode=sort_mode,
     )
     palette2 = extract_colors(
         image=test_image_path_as_str,
         palette_size=5,
-        mode="KM",
         resize=resize,
+        mode="KM",
         sort_mode=sort_mode,
     )
     for c1, c2 in zip(palette1.colors, palette2.colors):
