c7ea1adefee1c28ec8ab2742a54613758730d4b8
Ivar Stangeby <ivar.stangeby@cognite.com>
2025-01-27 23:11:12 +0100

    Implement median cut with a colorextractor class

--- Pylette/src/extractors/median_cut.py
+++ Pylette/src/extractors/median_cut.py
@@ -1,7 +1,35 @@
 import numpy as np
+from numpy.typing import NDArray
 
 from Pylette.src.color import Color
+from Pylette.src.extractors.protocol import NP_T, ColorExtractorBase
 from Pylette.src.utils import ColorBox
+
+
+class MedianCutExtractor(ColorExtractorBase):
+    def extract(self, arr: NDArray[NP_T], height: int, width: int, palette_size: int) -> list[Color]:
+        """
+        Extracts a color palette using the median cut algorithm.
+
+        Parameters:
+            arr (np.ndarray): The input array.
+            height (int): The height of the image.
+            width (int): The width of the image.
+            palette_size (int): The number of colors to extract from the image.
+
+        Returns:
+            list[Color]: A list of colors extracted from the image.
+        """
+
+        arr = self._reshape_array(arr=arr, height=height, width=width)
+
+        boxes = [ColorBox(arr)]
+        while len(boxes) < palette_size:
+            largest_box_idx = np.argmax(boxes)  # type: ignore
+            boxes = boxes[:largest_box_idx] + boxes[largest_box_idx].split() + boxes[largest_box_idx + 1 :]
+
+        total_pixels = width * height
+        return [Color(tuple(map(int, box.average)), box.pixel_count / total_pixels) for box in boxes]
 
 
 def median_cut_extraction(arr: np.ndarray, height: int, width: int, palette_size: int) -> list[Color]:
@@ -18,16 +46,4 @@ def median_cut_extraction(arr: np.ndarray, height: int, width: int, palette_size
         list[Color]: A list of colors extracted from the image.
     """
 
-    arr = arr.reshape((width * height, -1))
-    c = [ColorBox(arr)]
-
-    # Each iteration, find the largest box, split it, remove original box from list of boxes, and add the two new boxes.
-    while len(c) < palette_size:
-        largest_c_idx = np.argmax(c)
-        # add the two new boxes to the list, while removing the split box.
-        c = c[:largest_c_idx] + c[largest_c_idx].split() + c[largest_c_idx + 1 :]
-
-    total_pixels = width * height
-    colors = [Color(tuple(map(int, box.average)), box.pixel_count / total_pixels) for box in c]
-
-    return colors
+    return MedianCutExtractor().extract(arr, height, width, palette_size)
