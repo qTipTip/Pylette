4da8bf0859082f5faedae42738027c14aff0543c
Ivar Stangeby <istangeby@gmail.com>
2026-06-28 11:38:25 +0200

    add tests for basic registry ops

---
+++ tests/integration/test_extractor_registry.py
@@ -0,0 +1,25 @@
+import pytest
+
+from Pylette.src.extractors import available_methods, get_extractor
+from Pylette.src.extractors.protocol import ColorExtractor
+from Pylette.src.types import ExtractionMethod
+
+
+class TestRegistry:
+    def test_registry_and_enum_in_sync(self):
+        """Every enum member has an extractor and vice versa"""
+        assert set(available_methods()) == set(ExtractionMethod)
+
+    @pytest.mark.parametrize("method", list(ExtractionMethod))
+    def test_get_extractor_by_enum(self, method: ExtractionMethod):
+        extractor = get_extractor(method)
+        assert isinstance(extractor, ColorExtractor)
+
+    @pytest.mark.parametrize("method", list(ExtractionMethod))
+    def test_get_extractor_by_string(self, method: ExtractionMethod):
+        """Get extractor by string value should be identical to enum member"""
+        assert get_extractor(method.value) is get_extractor(method)
+
+    def test_unknown_method_raises_value_error(self):
+        with pytest.raises(ValueError):
+            get_extractor("NotARealMethod")
