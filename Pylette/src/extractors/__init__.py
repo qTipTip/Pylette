3104c81293c5f3b2c05d06430ae49e6515cd854a
Ivar Stangeby <istangeby@gmail.com>
2026-06-28 12:12:24 +0200

    register oklab

--- Pylette/src/extractors/__init__.py
+++ Pylette/src/extractors/__init__.py
@@ -1,6 +1,7 @@
 # Import for registration side-effect
 from Pylette.src.extractors import k_means as _k_means  # type: ignore # noqa: F401
 from Pylette.src.extractors import median_cut as _median_cut  # type: ignore  # noqa: F401
+from Pylette.src.extractors import oklab as _oklab  # type: ignore  # noqa: F401
 from Pylette.src.extractors.registry import available_methods, get_extractor, register
 
 __all__ = ["available_methods", "get_extractor", "register"]
